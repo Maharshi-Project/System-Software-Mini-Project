bb2b56636f81cfc2edbcbf2cb937f5ebcd20ebd0
Maharshi-Project <maharshipate20@gmail.com>
2024-01-15 14:59:00 +0530

    1st Final Commit

---
+++ Structures/courseStudent.h
@@ -0,0 +1,10 @@
+#ifndef COURSE_STUDENT_RECORD
+#define COURSE_STUDENT_RECORD
+
+struct courseStudent
+{
+	int studentID;
+	int courseID;	
+};
+
+#endif
---
+++ Structures/student.h
@@ -0,0 +1,18 @@
+#ifndef STUDENT_RECORD
+#define STUDENT_RECORD
+
+struct Student
+{
+    int id;
+    int active;		// 0 --> Deactive , 1 --> Activate
+    char name[40];
+    char gender;	// M --> Male, F --> Female, O --> Other
+    int age;		// Age of stundet
+    char email[40];
+    char address[200];
+    // Login Credentials
+    char login[30]; 	
+    char password[30];	
+};
+
+#endif
